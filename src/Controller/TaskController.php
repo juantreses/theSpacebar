2341f7667fda86c043f0e0acc938e953b4e38a29
juantreses <jandries@live.be>
2020-03-28 11:36:38 +0100

    api routing modified

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -22,64 +22,83 @@ class TaskController extends AbstractController
     }
 
     /**
-     * @Route("/api/taken")
+     * @Route("/api/taken", name="api_getTasks", methods={"GET"})
      */
-    public function taken()
+    public function getTaken()
     {
-        switch ($_SERVER['REQUEST_METHOD']) {
-            case 'GET' :
-                $stm = $this->pdo->prepare('SELECT * FROM taak');
-                $stm->execute();
+        $stm = $this->pdo->prepare('SELECT * FROM taak');
+        $stm->execute();
 
-                $rows = $stm->fetchAll(PDO::FETCH_ASSOC);
-                return new JsonResponse($rows);
-                break;
+        $rows = $stm->fetchAll(PDO::FETCH_ASSOC);
+        return new JsonResponse($rows);
 
-            case 'POST' :
-                $data = json_decode(file_get_contents("php://input"));
-
-                $sql = "INSERT INTO taak SET taa_datum = '$data->taa_datum', taa_omschr = '$data->taa_omschr'";
-
-                $stm = $this->pdo->prepare($sql);
-                if ($stm->execute()) {
-                    return new JsonResponse("Your task was added");
-                    break;
-                };
-
-        }
     }
 
     /**
-     * @Route("/api/taak/{id}")
+     * @return JsonResponse
+     * @Route("/api/taken", name="api_createTasks", methods={"POST"})
      */
-    public function taak($id)
+    public function createTask()
     {
-        switch ($_SERVER['REQUEST_METHOD']) {
-            case 'GET' :
-                $stm = $this->pdo->prepare("SELECT * FROM taak WHERE taa_id = $id");
-                $stm->execute();
+        $data = json_decode(file_get_contents("php://input"));
 
-                $rows = $stm->fetchAll(PDO::FETCH_ASSOC);
-                return new JsonResponse($rows);
-                break;
+        $sql = "INSERT INTO taak SET taa_datum = '$data->taa_datum', taa_omschr = '$data->taa_omschr'";
 
-            case 'PUT' :
-                $data = json_decode(file_get_contents("php://input"));
-
-                $sql = "Update taak SET taa_datum = '$data->taa_datum', taa_omschr = '$data->taa_omschr' WHERE taa_id = '$id'";
-
-                $stm = $this->pdo->prepare($sql);
-                if ($stm->execute()) {
-                    return new JsonResponse("Your task was updated");
-                    break;
-                }
-
-            case 'DELETE' :
-
-                $stm = $this->pdo->prepare("DELETE FROM taak WHERE taa_id = $id");
-                if ($stm->execute()) {
-                    return new JsonResponse("Your task was deleted");
-                }
+        $stm = $this->pdo->prepare($sql);
+        if ($stm->execute()) {
+            $response =  new JsonResponse("Your task was added");
         }
+        else {
+            $response = new JsonResponse("Something went wrong");
+        }
+
+        return $response;
+    }
+
+    /**
+     * @Route("/api/taak/{id}", name="api_getOneTask", methods={"GET"})
+     */
+    public function getTaak($id)
+    {
+        $stm = $this->pdo->prepare("SELECT * FROM taak WHERE taa_id = $id");
+        $stm->execute();
+
+        $rows = $stm->fetchAll(PDO::FETCH_ASSOC);
+        return new JsonResponse($rows);
+    }
+
+    /**
+     * @Route("/api/taak/{id}", name="api_updateTask", methods={"PUT"})
+     */
+    public function updateTaak($id)
+    {
+        $data = json_decode(file_get_contents("php://input"));
+
+        $sql = "Update taak SET taa_datum = '$data->taa_datum', taa_omschr = '$data->taa_omschr' WHERE taa_id = '$id'";
+
+        $stm = $this->pdo->prepare($sql);
+        if ($stm->execute()) {
+            $response = new JsonResponse("Your task was updated");
+        } else {
+            $response = new JsonResponse("Something went wrong");
+        }
+
+        return $response;
+    }
+
+    /**
+     * @Route("/api/taak/{id}", name="api_deleteTask", methods={"DELETE"})
+     */
+    public function deleteTaak($id)
+    {
+        $stm = $this->pdo->prepare("DELETE FROM taak WHERE taa_id = $id");
+        if ($stm->execute()) {
+            $response = new JsonResponse("Your task was deleted");
+        }
+        else {
+            $response = new JsonResponse("Something went wrong");
+        }
+
+        return $response;
     }
 }
